2899d558703aacf2ea20b8129ded3360814ea7ea
AndreaCorrado00 <corradoandrea00@gmail.com>
2024-07-11 17:44:24 +0200

    R2 analysis

--- main_preprocessing.m
+++ main_preprocessing.m
@@ -36,8 +36,44 @@ spaghetti_confidence_signals(data,fc,figure_path)
 % Comparison between different signals mean/periodogram traces for the same case
 compare_case_signals(data,fc,figure_path)
 
+<<<<<<< Updated upstream
 % Comparison within traces between subjects
+<<<<<<< Updated upstream
 compare_traces_between_sub(data,fc,figure_path)
+=======
+table_pox=[false,true;
+            true,true];
+=======
+%% 2.4 Similarity between signals 
+table_corr=correlation_signals_within_maps(data,'rov_trace',true);
+% It could be interesting looking at the correlation values within
+% subjects. IE a sort of distribution
+>>>>>>> Stashed changes
+
+type_plots=["comp_case_by_sign"; "comp_case_spectrum_by_sign"];
+for l = 1:2
+    for i = ["A","B","C"]
+        map = 'MAP_' + i;
+        subjects = fieldnames(data.(map));
+        for k = ["rov","ref","spare1","spare2","spare3"]
+            for j = 1:length(subjects)
+                sub = map + num2str(j);
+                trace = k + '_trace';
+                title_plot = 'MAP:' + i + ' (' + get_name_of_map(i) + '), trace:' + k + ', subjects comparison';
+                fig = figure(1);
+                fig.WindowState = "maximized";
+                hold on
+                compare_by_plotting_signals(data.(map).(sub).(trace), title_plot, fc, table_pox(1, l), table_pox(2, l));
+                
+                legend_entries{j} = ['Subject ' num2str(j)];
+            end  
+            legend(legend_entries);
+            hold off
+            save_plot(i, '', k, type_plots(l), figure_path+"\traces_comp", fig, true);
+        end
+    end
+end
+>>>>>>> Stashed changes
 
 % comparison between maps within subjects and traces
 compare_maps_between_signals(data,fc,figure_path)
---
+++ src/correlation_signals_within_maps.m
@@ -0,0 +1,25 @@
+function table_corr=correlation_signals_within_maps(data,trace,display)
+
+
+
+for i=1:length(fieldnames((data.MAP_A)))
+    table_sub=[];
+    for j=1:3
+        for k=1:3
+            subA="MAP_A"+num2str(i);
+            subB="MAP_B"+num2str(i);
+            subC="MAP_C"+num2str(i);
+            mean_signals=[table2array(mean(data.MAP_A.(subA).(trace),2)),table2array(mean(data.MAP_B.(subB).(trace),2)),table2array(mean(data.MAP_C.(subC).(trace),2))];
+            table_sub(j,k)=corr(mean_signals(:,j),mean_signals(:,k));
+
+        end
+    end
+    table_sub=array2table(table_sub);
+    table_sub.Properties.VariableNames={char("A "+trace);char("B "+trace);char("C "+trace)};
+    table_sub.Properties.RowNames={char("A "+trace);char("B "+trace);char("C "+trace)};
+    table_corr.("sub_"+num2str(i))=table_sub;
+end
+
+
+end
+
