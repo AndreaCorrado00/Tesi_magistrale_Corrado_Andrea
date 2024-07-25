54d91f3f7013182ac30cea65c910d7df61b22eb9
AndreaCorrado00 <corradoandrea00@gmail.com>
2024-07-25 11:07:13 +0200

    useful images dir added

--- src/Preprocessing_phase/evaluate_order.m
+++ src/Preprocessing_phase/evaluate_order.m
@@ -6,7 +6,7 @@ p_opt=0;
 for i=1:length(p)
     th=ar(table2array(signal)-table2array(mean(signal)),p(i),'ls');
     AIC_vec(i)=aic(th,'aic');
-    
+
     if i ~=1 && abs(AIC_vec(i-1)-AIC_vec(i))<eps
         pos_opt=i;
     end
@@ -15,6 +15,5 @@ if p_opt==0
     pos_opt=find(AIC_vec==min(AIC_vec));
 end
 p_opt=p(pos_opt);
-
 
 end
