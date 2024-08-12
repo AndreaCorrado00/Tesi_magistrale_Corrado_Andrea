71d2a19a6dd296fc621cfbe2e5b59976389dc98d
AndreaCorrado00 <corradoandrea00@gmail.com>
2024-08-12 12:27:13 +0200

    figures after cleaning

--- src/Spectrum_comparison_phase/filter_signals.m
+++ src/Spectrum_comparison_phase/filter_signals.m
@@ -25,20 +25,20 @@ for i=1:length(subjects)
     [n,Wp]=ellipord(Wp,Ws,Rp,Rs);
     [b,a]=ellip(n,Rp,Rs,Wp);
     % 
-    % %% risposta in frequenza del filtro
-    % [H,f]=freqz(b,a,512,Fc);
-    % % mostriamola
-    % figure(1)
-    % subplot(211)
-    % plot(f,abs(H))
-    % 
-    % grid on
-    % title('Modulo del filtro')
-    % 
-    % subplot(212)
-    % plot(f,angle(H))
-    % grid on
-    % title('Sfasamento ')
+    %% risposta in frequenza del filtro
+    [H,f]=freqz(b,a,512,Fc);
+    % mostriamola
+    figure(1)
+    subplot(211)
+    plot(f,abs(H))
+
+    grid on
+    title('Modulo del filtro')
+
+    subplot(212)
+    plot(f,angle(H))
+    grid on
+    title('Sfasamento ')
     data_filtered.(sub).lead_1_data=filter(b,a,data.(sub).lead_1_data-mean(data.(sub).lead_1_data));
     
 end
