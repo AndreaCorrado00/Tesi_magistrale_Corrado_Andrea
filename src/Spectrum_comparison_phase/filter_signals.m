5d2f70aa94f5781847a9748db1ccffe7850768d1
AndreaCorrado00 <corradoandrea00@gmail.com>
2024-08-13 15:21:06 +0200

    new spectrum eval images, finished phase

--- src/Spectrum_comparison_phase/filter_signals.m
+++ src/Spectrum_comparison_phase/filter_signals.m
@@ -8,55 +8,40 @@ for i=1:length(subjects)
 
     %% Filter proprieties
     Fc=data.(sub).fc; %Hz
-    % %% fixing filtering options: Notch
-    % % filter parameters in Hz
-    % Wp=1;%Hz
-    % Ws=Wp-Wp/4; %da progetto la banda di transizione deve essere pari a 1/4 di Wp
-    % % quindi Wp-Ws=1/4Wp => Ws=Wp-1/4Wp
+
+    %% fixing filtering options: Band Pass
+  
+    % Wp = [1 40] / (Fc / 2);  % Banda passante normalizzata (da 0.5 Hz a 40 Hz)
+    % Rp = 0.95;  % Ripple nella banda passante (dB)
+    % Ws = [0.1 60] / (Fc / 2);  % Frequenze di stop normalizzate (un po' più ampie per transizione)
+    % Rs = 0.1;   % Attenuazione nella banda eliminata (dB)
     % 
-    % Rp=0.90; % in percentuale
-    % Rs=0.10;
-    % 
-    % % parameters conversions
-    % Wp=Wp/(Fc/2);
-    % Ws=Ws/(Fc/2);
     % Rp=-20*log10(Rp);
     % Rs=-20*log10(Rs);
-    % 
-    % 
-    % %% filter determination
-    % [n,Wp]=ellipord(Wp,Ws,Rp,Rs);
-    % [b,a]=ellip(n,Rp,Rs,Wp,"high");
-    % 
-    % %% risposta in frequenza del filtro
-    % [H,f]=freqz(b,a,512,Fc);
-    % % mostriamola
-    % figure(1)
-    % subplot(211)
-    % plot(f,abs(H))
-    % 
-    % grid on
-    % title('Modulo del filtro PA')
-    % 
-    % subplot(212)
-    % plot(f,angle(H))
-    % grid on
-    % title('Sfasamento ')
-    % data_filtered.(sub).lead_1_data=filter(b,a,data.(sub).lead_1_data-mean(data.(sub).lead_1_data));
+    % [n, Wn] = cheb1ord(Wp, Ws, Rp, Rs);  % Ordine e frequenze del filtro
+    % [b, a] = cheby1(n, Rp, Wn,"high");  % Coefficienti del filtro di Chebyshev di tipo I
 
-    %% fixing filtering options: Low Pass
-  
-    Wp = [1 40] / (Fc / 2);  % Banda passante normalizzata (da 0.5 Hz a 40 Hz)
-    Rp = 0.95;  % Ripple nella banda passante (dB)
-    Ws = [0.1 60] / (Fc / 2);  % Frequenze di stop normalizzate (un po' più ampie per transizione)
-    Rs = 0.1;   % Attenuazione nella banda eliminata (dB)
+    Wp=0.5;%Hz
+    Ws=(Wp-Wp/4); %da progetto la banda di transizione deve essere pari a 1/4 di Wp
+    % quindi Wp-Ws=1/4Wp => Ws=Wp-1/4Wp
 
+    Rp=0.90; % in percentuale
+    Rs=0.10;
+
+
+    %% Parametri convertiti
+    Wp=Wp/(Fc/2);
+    Ws=Ws/(Fc/2);
     Rp=-20*log10(Rp);
     Rs=-20*log10(Rs);
-    [n, Wn] = cheb1ord(Wp, Ws, Rp, Rs);  % Ordine e frequenze del filtro
-    [b, a] = cheby1(n, Rp, Wn,"bandpass");  % Coefficienti del filtro di Chebyshev di tipo I
 
-
+    %% determinazione del filtro
+    [n,Wn]=ellipord(Wp,Ws,Rp,Rs); %la frequenza di taglio coincede con quella di
+    %inizio banda oscura per una
+    % decisione implementativa interna di
+    % matlab. In genere, anche nella teoria, è
+    % così.
+    [b,a]=ellip(n,Rp,Rs,Wn,"high");
     %% risposta in frequenza del filtro
     [H,f]=freqz(b,a,512,Fc);
     % mostriamola
@@ -65,12 +50,13 @@ for i=1:length(subjects)
     plot(f,abs(H))
 
     grid on
-    title('Modulo del filtro PB')
-
+    title('Elliptical High Pass Filter')
+    ylabel('Magnitude')
     subplot(212)
     plot(f,angle(H))
     grid on
-    title('Sfasamento ')
+    ylabel('Phase')
+    xlabel('Normalized frequency')
     data_filtered.(sub).lead_1_data=filter(b,a,data.(sub).lead_1_data-mean(data.(sub).lead_1_data));
     
 end
