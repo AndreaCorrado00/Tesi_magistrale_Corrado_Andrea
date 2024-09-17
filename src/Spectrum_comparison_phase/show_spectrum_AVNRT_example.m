3aca7186d389bdbb5d16a5335fe49532a0d2034f
AndreaCorrado00 <corradoandrea00@gmail.com>
2024-09-17 15:27:20 +0200

    new images

--- src/Spectrum_comparison_phase/show_spectrum_AVNRT_example.m
+++ src/Spectrum_comparison_phase/show_spectrum_AVNRT_example.m
@@ -15,61 +15,158 @@ function show_spectrum_AVNRT_example(data)
 %   methods from the provided data structure. It compares their power spectra
 %   using AR estimation, Welch periodogram, and FFT.
 
+
 % Extraction of signals filtered with DWT
-rov = data.DWT.rov_trace(:,1);
-ref = data.DWT.ref_trace(:,1);
-spare1 = data.DWT.spare1_trace(:,1);
-spare2 = data.DWT.spare2_trace(:,1);
-spare3 = data.DWT.spare3_trace(:,1);
+rov=data.DWT.rov_trace(:,1);
+ref=data.DWT.ref_trace(:,1);
+spare1=data.DWT.spare1_trace(:,1);
+spare2=data.DWT.spare2_trace(:,1);
+spare3=data.DWT.spare3_trace(:,1);
+
+tab_signals_DWT=[rov,ref,spare1,spare2,spare3];
 
 % Extraction of signals filtered with BP
-rov_BP = data.BP.rov_trace(:,1);
-ref_BP = data.BP.ref_trace(:,1);
-spare1_BP = data.BP.spare1_trace(:,1);
-spare2_BP = data.BP.spare2_trace(:,1);
-spare3_BP = data.BP.spare3_trace(:,1);
+rov=data.BP.rov_trace(:,1);
+ref=data.BP.ref_trace(:,1);
+spare1=data.BP.spare1_trace(:,1);
+spare2=data.BP.spare2_trace(:,1);
+spare3=data.BP.spare3_trace(:,1);
 
-% Parameters
-Fs = 2035; % Sampling frequency
-signals = [rov, ref, spare1, spare2, spare3];
-signals_BP = [rov_BP, ref_BP, spare1_BP, spare2_BP, spare3_BP];
-traces = ["Rov trace", "Ref trace", "Spare1 trace", "Spare2 trace", "Spare3 trace"];
-colors = ["b", "r", "g", "c", "m"];
+tab_signals_PB=[rov,ref,spare1,spare2,spare3];
 
-figure(1)
-sgtitle('Comparison of Power Spectra (DWT vs BP)')
+traces=["Rov trace","Ref trace","Spare1 trace","Spare2 trace","Spare3 trace"];
+Fs=2035;
 
-for i = 1:5
-    x = signals(:,i) - mean(signals(:,i));
-    x_BP = signals_BP(:,i) - mean(signals_BP(:,i));
-    color = colors(i);
+figure(2)
+sgtitle('Power spectrum estimation, DWT filter signals')
 
-    % Plot for DWT Filter
-    subplot(5,2,2*i-1)
-    title([traces(i), ' DWT'])
-    p = 10;
-    th = ar(x, p, 'burg');
-    [H, f] = freqz(1, th.a, length(x), Fs);
+for i=1:5
+    x_w=tab_signals_DWT(:,i)-mean(tab_signals_DWT(:,i));
+    N = length(x_w);
+
+    %% AR spectrum estimation (ls)
+    p = evaluate_order(x_w, 8, 20, 2, 6,'ls');
+    th =  ar(x_w-mean(x_w), p, 'ls');
+    [H, f] = freqz(1, th.a, N, Fs);
+    f_DSP = f;
+    DSP = th.NoiseVariance * (abs(H).^2);
+
+    %% AR spectrum estimation (burg)
+    p_bu =evaluate_order(x_w, 8, 16, 2, 7,'burg');
+    th= ar(x_w-mean(x_w), p_bu, 'burg');
+    [H, f] = freqz(1, th.a, N, Fs);
     f_BU = f;
     DSP_BU = th.NoiseVariance * (abs(H).^2);
-    plot(f_BU, DSP_BU, 'Color', color, "LineWidth", 1.5)
-    xlabel('f [Hz]')
-    ylabel('Amplitude')
-    xlim([0, 60])
-    ylim([0, 0.2])
 
-    % Plot for BP Filter
-    subplot(5,2,2*i)
-    title([traces(i), ' BP'])
-    p = 10;
-    th = ar(x_BP, p, 'burg');
-    [H, f] = freqz(1, th.a, length(x_BP), Fs);
-    f_BU = f;
-    DSP_BU = th.NoiseVariance * (abs(H).^2);
-    plot(f_BU, DSP_BU, 'Color', color, "LineWidth", 1.5)
+
+    % Normalization
+    U=max(DSP)/max(DSP_BU);
+    DSP_BU=U.*DSP_BU;
+
+    %% Welch spectrum estimation
+    window = hamming(512); % Hamming window
+    noverlap = length(window) / 2; % overlapping
+    nfft = 2048; % Points of fft
+
+    % Welch periodogram
+    [pxx, f] = pwelch(x_w, window, noverlap, nfft, Fs);
+
+    % Normalization
+    U = max(DSP) / max(pxx);
+
+    pxx = U * pxx;
+
+    %% Spectrogram estimation
+    FT_x=fft(x_w,N);
+    S=abs(FT_x).^2/N;
+    f_S=0:Fs/N:Fs-Fs/N;
+
+    % Normalization
+    U=max(DSP)/max(S);
+    S=U.*S;
+
+
+    %% Results
+    subplot(3,2,i)
+    hold on
+    plot(f, pxx,'Color',[0.8500 0.3250 0.0980],'LineWidth',0.5)
+    plot(f_S,S,'Color',[0.9290 0.6940 0.1250],'LineWidth',0.5)
+    plot(f_DSP, DSP,'Color',[0.4940 0.1840 0.5560],'LineWidth',1.5)
+   % plot(f_BU, DSP_BU,'Color',[0 0.4470 0.7410],'LineWidth',1.5)
+    hold off
+    ylabel('PSD')
     xlabel('f [Hz]')
-    ylabel('Amplitude')
-    xlim([0, 60])
-    ylim([0, 0.2])
+    title(traces(i)+", p_{AR}(LS)=" + num2str(p))
+    xlim([0, 200])
+    legend( 'Welch','FFT','AR LS estimation')
+    hold on
+
 end
+
+
+figure(3)
+sgtitle('Power spectrum estimation, BP filter signals')
+for i=1:5
+    x_w=tab_signals_PB(:,i)-mean(tab_signals_PB(:,i));
+    N=length(x_w);
+    %% AR spectrum estimation (ls)
+    p = evaluate_order(x_w, 8, 20, 2, 6,'ls');
+    th =  ar(x_w-mean(x_w), p, 'ls');
+    [H, f] = freqz(1, th.a, N, Fs);
+    f_DSP = f;
+    DSP = th.NoiseVariance * (abs(H).^2);
+
+    %% AR spectrum estimation (burg)
+    p_bu =evaluate_order(x_w, 8, 16, 2, 7,'burg');
+    th= ar(x_w-mean(x_w), p_bu, 'burg');
+    [H, f] = freqz(1, th.a, N, Fs);
+    f_BU = f;
+    DSP_BU = th.NoiseVariance * (abs(H).^2);
+
+
+    % Normalization
+    U=max(DSP)/max(DSP_BU);
+    DSP_BU=U.*DSP_BU;
+
+    %% Welch spectrum estimation
+    window = hamming(512); % Hamming window
+    noverlap = length(window) / 2; % overlapping
+    nfft = 2048; % Points of fft
+
+    % Welch periodogram
+    [pxx, f] = pwelch(x_w, window, noverlap, nfft, Fs);
+
+    % Normalization
+    U = max(DSP) / max(pxx);
+
+    pxx = U * pxx;
+
+    %% Spectrogram estimation
+    FT_x=fft(x_w,N);
+    S=abs(FT_x).^2/N;
+    f_S=0:Fs/N:Fs-Fs/N;
+
+    % Normalization
+    U=max(DSP)/max(S);
+    S=U.*S;
+
+
+    %% Results
+    subplot(3,2,i)
+    hold on
+    plot(f, pxx,'Color',[0.8500 0.3250 0.0980],'LineWidth',0.5)
+    plot(f_S,S,'Color',[0.9290 0.6940 0.1250],'LineWidth',0.5)
+    plot(f_DSP, DSP,'Color',[0.4940 0.1840 0.5560],'LineWidth',1.5)
+    %plot(f_BU, DSP_BU,'Color',[0 0.4470 0.7410],'LineWidth',1.5)
+    hold off
+    ylabel('PSD')
+    xlabel('f [Hz]')
+    title(traces(i)+", p_{AR}(LS)=" + num2str(p))
+    xlim([0, 200])
+    legend( 'Welch','FFT','AR LS estimation')
+    hold on
+
+end
+
+
 end
