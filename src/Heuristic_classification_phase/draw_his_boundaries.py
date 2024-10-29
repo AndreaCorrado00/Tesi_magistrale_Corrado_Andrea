ddd91bd8880b4353bd3354acda95f58aa364ead8
AndreaCorrado00 <corradoandrea00@gmail.com>
2024-10-29 14:42:13 +0100

    ppts heuristic done

--- src/Heuristic_classification_phase/draw_his_boundaries.py
+++ src/Heuristic_classification_phase/draw_his_boundaries.py
@@ -3,8 +3,8 @@ def draw_his_boundaries(atr_t,ven_t, th_his=None):
     import numpy as np
     
     if th_his is not None:
-        plt.plot([atr_t, ven_t], [th_his, th_his], color='grey', linestyle=':')
-        plt.plot([atr_t, ven_t], [-th_his, -th_his], color='grey', linestyle=':')
+        plt.plot([atr_t, ven_t], [th_his, th_his], color='red', linestyle='--')
+        plt.plot([atr_t, ven_t], [-th_his, -th_his], color='red', linestyle='--')
         plt.plot([atr_t, atr_t], [th_his, -th_his],color='grey', linestyle=':');
         plt.plot([ven_t, ven_t], [th_his, -th_his],color='grey', linestyle=':');
         
