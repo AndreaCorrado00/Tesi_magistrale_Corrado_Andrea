ed738df133f36b660e15a0934140336068c63e9d
AndreaCorrado00 <corradoandrea00@gmail.com>
2025-02-07 09:30:07 +0100

    presentation done

--- src/Envelope_template_STFT_phase/build_features_set.m
+++ src/Envelope_template_STFT_phase/build_features_set.m
@@ -30,7 +30,7 @@ for i = ["A", "B", "C"]
     subjects = fieldnames(data.(map));
 
     % Loop through each subject
-    for j = 1:length(subjects)
+    for j =1:length(subjects)
         sub = subjects{j};
 
         % Get the number of signals for the current subject
